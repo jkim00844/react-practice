381a921ca9b0b36466917dfe917fe07852b10ab4
김지영 <jiyeongkim@gimjiyeong-ui-MacBookPro-2.local>
2021-12-16 17:21:26 +0900

    #4 Landing Page 만들기

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -2,5 +2,5 @@
 export const USER_SERVER = '/api/users';
 
 export const API_URL = 'https://api.themoviedb.org/3/';
-
+export const API_KEY = '43702f9c0ed271e62bc31c9d7ebe4905';
 export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,56 @@
-import React from 'react'
+// import { response } from 'express';
+import React, { useEffect, userState, useState } from 'react'
 import { FaCode } from "react-icons/fa";
+import {API_URL, API_KEY, IMAGE_BASE_URL} from '../../Config';
+import MainImage from './Sections/MainImage';
 
 function LandingPage() {
+
+    const [Movies, setMovies] = useState([]);
+    const [MainMovieImage, setMainMovieImage] = useState(null);
+
+    useEffect(()=> {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
+        fetch(endpoint)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response);
+            // useState 에 Movies 추가
+            setMovies([response.results])
+            setMainMovieImage(response.results[0])
+        })
+        
+        
+    }, []);
+
+
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+        <div style={{width: '100%', margin: '0'}}>
+            {/*Main Image*/}
+
+            {/*functional component, prop 사용 */}
+            {/*Cannot read properties of null (reading 'backdrop_path') */}
+            {/*MainMovieImage를 설정하기 전에 페이지가 랜더되서 그럼 MainMovieImage && 이걸로 MainMovieImage불러오면 실행되게*/}
+
+            {MainMovieImage &&
+                <MainImage 
+                image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+                title={MainMovieImage.original_title}
+                text={MainMovieImage.overview}
+                /> 
+            }
+            <div style={{width: '85%', marin: '1rem auto'}}>
+                <h2>Movies by latest</h2>
+                <hr />
+
+                { /*Movie Grid Cards*/}
             </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+
+            <div style={{display: 'felx', justifyContent: 'center' }}>
+                <button>Load More</button>
+            </div>
+        </div>
     )
 }
 
