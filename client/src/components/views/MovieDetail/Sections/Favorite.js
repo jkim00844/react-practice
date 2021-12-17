a0bd3c14468c269d7a6144ea85547c04d8542bee
김지영 <jkim00844@gmail.com>
2021-12-17 15:01:43 +0900

    #11 Favorite 버튼 만들기(3)

--- client/src/components/views/MovieDetail/Sections/Favorite.js
+++ client/src/components/views/MovieDetail/Sections/Favorite.js
@@ -1,37 +1,45 @@
-import Axios from 'axios'
-import React, { useEffect } from 'react'
+import Axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function Favorite(props) {
+  const movieId = props.movieId;
+  const userFrom = props.userFrom;
+  const movieTitle = props.movieInfo.title;
+  const moviePost = props.movieInfo.back_drop_path;
+  const movieRunTime = props.movieInfo.runtime;
+
+  const [FavoriteNumber, setFavoriteNumber] = useState(0)
+  const [Favorited, setFavorited] = useState(false)
+
+  useEffect(() => {
+    let variables = {
+      userFrom,
+      movieId,
+    };
+    Axios.post("/api/favorite/favoriteNumber", variables).then((response) => {
+    //   console.log(response.data);
+      if (response.data.success) {
+        setFavoriteNumber(response.data.favoriteNumber)
+      } else {
+        alert("숫자 정보를 가져오는 데 실패 했습니다.");
+      }
+    });
     
-    const movieId = props.movieId
-    const userFrom = props.userFrom
-    const movieTitle = props.movieInfo.title
-    const moviePost = props.movieInfo.back_drop_path
-    const movieRunTime = props.movieInfo.runtime
+    Axios.post("/api/favorite/favorited", variables).then((response) => {
+    //   console.log(response.data);
+      if (response.data.success) {
+        setFavorited(response.data.favorited)
+    } else {
+        alert("정보를 가져오는 데 실패 했습니다.");
+      }
+    });
+  }, []);
 
-    useEffect(() => {
-        
-        let variables ={
-            userFrom,
-            movieId,
-        }
-        Axios.post('/api/favorite/favoriteNumber', variables)
-            .then(response => {
-                console.log(response.data);
-                if(response.data.success){
-                    
-                }else{
-                    alert('숫자 정보를 가져오는 데 실패 했습니다.')
-                }
-            })
-
-    }, [])
-
-    return (
-        <div>
-            <button>Favorite</button>
-        </div>
-    )
+  return (
+    <div>
+      <button>{Favorited ? "Not Favorite" : "Add to Favorite"} {FavoriteNumber}</button>
+    </div>
+  );
 }
 
-export default Favorite
+export default Favorite;
--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -10,7 +10,24 @@ router.post('/favoriteNumber', (req, res) => {
             if(err) return res.status(400).send(err);
             // 좋아요 개수만 넘기기
             // 그다음 프론트에 다시 넘겨주기
-            res.status(200).json({ success: true, favorieNumber : info.length})
+            res.status(200).json({ success: true, favoriteNumber : info.length})
+        })
+})
+
+router.post('/favorited', (req, res) => {
+
+    // 내가 이 영화를 Favorite 리스트에 넣었는지 정보를 DB에서 가져오기
+    // monogoDB에서 favorite 숫자가져오기
+    Favorite.find({"movieId" : req.body.movieId,  "userFrom" : req.body.userFrom })
+        .exec((err, info) =>{
+            if(err) return res.status(400).send(err);
+            
+            let result = false;
+            if(info.length !== 0 ){
+                result = true;
+            }
+            // 그다음 프론트에 다시 넘겨주기
+            res.status(200).json({ success: true, favorited : result })
         })
 })
 
