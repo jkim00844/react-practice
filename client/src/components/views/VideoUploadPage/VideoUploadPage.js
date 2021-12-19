57d0da93d2cc43d79770c931e77e48da28b1b26d
김지영 <jkim00844@gmail.com>
2021-12-20 00:09:40 +0900

    #4 비디오 업로드 form 만들기(2)

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { Typography, Button, Form, message, Input, Icon } from "antd";
 import Dropzone from "react-dropzone";
 
 const { TextArea } = Input;
 const { Title } = Typography;
 
+const PrivateOptions = [
+    { value: 0, label: "private" },
+    { value: 1, label: "public" }
+]
+
+const CategoryOptions = [
+    { value: 0, label: "Film & Animation" },
+    { value: 1, label: "Autos & Vehicles" },
+    { value: 2, label: "Music" },
+    { value: 3, label: "Pets & Animals" }
+]
+
 function VideoUploadPage() {
-  return (
+  
+    const [VideoTitle, setVideoTitle] = useState("")
+    const [Description, setDescription] = useState("") 
+    const [Private, setPrivate] = useState(0)
+    const [Category, seTCategory] = useState("Film & Animation")
+
+    const onTitleChange = (e) => {
+        setVideoTitle(e.currentTarget.value)
+    }
+
+    const onDescriptitionChange = (e) => {
+        setDescription(e.currentTarget.value)
+    }
+
+    const onCategoryChange = (e) => {
+        setDescription(e.currentTarget.value)
+    }
+
+    const onPrivateChange = (e) => {
+        setDescription(e.currentTarget.value)
+    }
+
+    return (
     <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
       <div style={{ textAlign: "center", marginBottom: "2rem" }}>
         <Title level={2}>Upload Video</Title>
@@ -15,19 +49,23 @@ function VideoUploadPage() {
       <Form onSubmit>
         <div style={{ display: "flex", justifyContent: "space-between" }}>
           {/* Drop Zone */}
-          <Dropzone
-          onDrop
-          multiple
-          maxSize
-          >
-            {({ getRootProps, getInputProps}) => (
-                <div style={{ width: '300px', height: '240px', border: '1px solid lightgray', display:'flex',
-                    alignItems: 'center', justifyContent:'center'}} {...getRootProps()}>
-                    <input {...getInputProps()}/>
-                    <Icon type ="plus" style={{ fontSize:'3rem'}} />
-                </div>
+          <Dropzone onDrop multiple maxSize>
+            {({ getRootProps, getInputProps }) => (
+              <div
+                style={{
+                  width: "300px",
+                  height: "240px",
+                  border: "1px solid lightgray",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+                {...getRootProps()}
+              >
+                <input {...getInputProps()} />
+                <Icon type="plus" style={{ fontSize: "3rem" }} />
+              </div>
             )}
-
           </Dropzone>
 
           {/* Thumbnail Zone */}
@@ -40,26 +78,30 @@ function VideoUploadPage() {
         <br />
 
         <label>Title</label>
-        <Input onChange value />
+        <Input onChange={onTitleChange} value={VideoTitle} />
 
         <br />
         <br />
 
         <label>Description</label>
-        <TextArea onChange value />
+        <TextArea onChange={onDescriptitionChange} value={Description}/>
 
         <br />
         <br />
 
-        <select onChange>
-          <option key value></option>
+        <select onChange={onPrivateChange}>
+            { PrivateOptions.map((item, index) => (
+                <option key={item} value={item.value}>{item.label}</option>
+            ))}
         </select>
 
         <br />
         <br />
 
-        <select onChange>
-          <option key value></option>
+        <select onChange={onCategoryChange}>
+            { CategoryOptions.map((item, index) => (
+                <option key={item} value={item.value}>{item.label}</option>
+            ))}
         </select>
 
         <br />
