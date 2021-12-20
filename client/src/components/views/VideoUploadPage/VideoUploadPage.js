f74af4241adc55376db6fcef246043ab24bace3a
김지영 <jkim00844@gmail.com>
2021-12-20 12:25:54 +0900

    #5 Multer로 노드 서버에 비디오 저장하기

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,46 +1,67 @@
 import React, { useState } from "react";
 import { Typography, Button, Form, message, Input, Icon } from "antd";
 import Dropzone from "react-dropzone";
+import Axios from "axios";
 
 const { TextArea } = Input;
 const { Title } = Typography;
 
 const PrivateOptions = [
-    { value: 0, label: "private" },
-    { value: 1, label: "public" }
-]
+  { value: 0, label: "private" },
+  { value: 1, label: "public" },
+];
 
 const CategoryOptions = [
-    { value: 0, label: "Film & Animation" },
-    { value: 1, label: "Autos & Vehicles" },
-    { value: 2, label: "Music" },
-    { value: 3, label: "Pets & Animals" }
-]
+  { value: 0, label: "Film & Animation" },
+  { value: 1, label: "Autos & Vehicles" },
+  { value: 2, label: "Music" },
+  { value: 3, label: "Pets & Animals" },
+];
 
+// http://localhost:3000/video/upload
 function VideoUploadPage() {
-  
-    const [VideoTitle, setVideoTitle] = useState("")
-    const [Description, setDescription] = useState("") 
-    const [Private, setPrivate] = useState(0)
-    const [Category, seTCategory] = useState("Film & Animation")
+  const [VideoTitle, setVideoTitle] = useState("");
+  const [Description, setDescription] = useState("");
+  const [Private, setPrivate] = useState(0);
+  const [Category, setCategory] = useState("Film & Animation");
 
-    const onTitleChange = (e) => {
-        setVideoTitle(e.currentTarget.value)
-    }
+  const onTitleChange = (e) => {
+    setVideoTitle(e.currentTarget.value);
+  };
 
-    const onDescriptitionChange = (e) => {
-        setDescription(e.currentTarget.value)
-    }
+  const onDescriptitionChange = (e) => {
+    setDescription(e.currentTarget.value);
+  };
 
-    const onCategoryChange = (e) => {
-        setDescription(e.currentTarget.value)
-    }
+  const onCategoryChange = (e) => {
+    setCategory(e.currentTarget.value);
+  };
 
-    const onPrivateChange = (e) => {
-        setDescription(e.currentTarget.value)
-    }
+  const onPrivateChange = (e) => {
+    setPrivate(e.currentTarget.value);
+  };
 
-    return (
+  const onDrop = (files) => {
+    let formData = new FormData();
+
+    const config = {
+      header: { "content-type": "nultipart/form-data" },
+    };
+    // console.log(files);
+
+    formData.append("file", files[0]);
+
+    Axios.post("/api/video/uploadfiles", formData, config).then((response) => {
+      if (response.data.success) {
+        console.log(response.data);
+      } else {
+        alert("비디오 업로드를 실패하였습니다.");
+      }
+    });
+
+  };
+
+  return (
     <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
       <div style={{ textAlign: "center", marginBottom: "2rem" }}>
         <Title level={2}>Upload Video</Title>
@@ -49,7 +70,12 @@ function VideoUploadPage() {
       <Form onSubmit>
         <div style={{ display: "flex", justifyContent: "space-between" }}>
           {/* Drop Zone */}
-          <Dropzone onDrop multiple maxSize>
+          <Dropzone
+            onDrop={onDrop}
+            // 파일 여러개 올릴지 말지
+            multiple={false}
+            maxSize={100000000000000}
+          >
             {({ getRootProps, getInputProps }) => (
               <div
                 style={{
@@ -84,24 +110,28 @@ function VideoUploadPage() {
         <br />
 
         <label>Description</label>
-        <TextArea onChange={onDescriptitionChange} value={Description}/>
+        <TextArea onChange={onDescriptitionChange} value={Description} />
 
         <br />
         <br />
 
         <select onChange={onPrivateChange}>
-            { PrivateOptions.map((item, index) => (
-                <option key={item} value={item.value}>{item.label}</option>
-            ))}
+          {PrivateOptions.map((item, index) => (
+            <option key={item} value={item.value}>
+              {item.label}
+            </option>
+          ))}
         </select>
 
         <br />
         <br />
 
         <select onChange={onCategoryChange}>
-            { CategoryOptions.map((item, index) => (
-                <option key={item} value={item.value}>{item.label}</option>
-            ))}
+          {CategoryOptions.map((item, index) => (
+            <option key={item} value={item.value}>
+              {item.label}
+            </option>
+          ))}
         </select>
 
         <br />
